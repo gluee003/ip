7883950e2d2c9dd87645b3df048b97145c4c30ba
gluee003 <lzhlzhlzh123@gmail.com>
2026-01-22 05:27:36 +0800

    Improve bot to echo commands

--- src/main/java/Bru.java
+++ src/main/java/Bru.java
@@ -1,9 +1,25 @@
+import java.util.Scanner;
+
 public class Bru {
     public static final String NAME = "Bru";
 
     public static void main(String[] args) {
         System.out.println("Hello! I'm " + Bru.NAME);
         System.out.println("What can I do for you?\n");
+
+        Scanner scanner = new Scanner(System.in);
+        boolean isChatting = true;
+        while (isChatting) {
+            String input = scanner.nextLine();
+            switch (input) {
+            case "bye":
+                isChatting = false;
+                break;
+            default:
+                System.out.println(input + "\n");
+                break;
+            }
+        }
         System.out.println("Bye. Hope to see you again soon!");
     }
 }
