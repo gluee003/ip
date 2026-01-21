a4729b10af8ac1a33f3738e5de2190aea9cca054
gluee003 <lzhlzhlzh123@gmail.com>
2026-01-22 05:04:46 +0800

    Add skeletal version

---
+++ src/main/java/Bru.java
@@ -0,0 +1,9 @@
+public class Bru {
+    public static final String NAME = "Bru";
+
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm " + Bru.NAME);
+        System.out.println("What can I do for you?\n");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
